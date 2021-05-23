eb21fb754331781a95c561018d91b4924a8365db
edifus <edifus@gmail.com>
2021-05-23 14:57:44 -0400

    cleanup and typos

--- plotmanager/library/utilities/dashboard.py
+++ plotmanager/library/utilities/dashboard.py
@@ -39,7 +39,7 @@ def dashboard_thread():
 def dashboard_update_loop():
     try:
         while True:
-            dashboard_logging.info("Starting update ...")
+            dashboard_logging.debug("Starting update ...")
             update_dashboard()
             dashboard_logging.debug("Sleeping 60 seconds ...")
             time.sleep(60)
@@ -98,7 +98,7 @@ def get_job_data(jobs, running_work, analysis):
     rows.sort(key=lambda x: (x[4]), reverse=True)
     for i in range(len(rows)):
         rows[i] = [str(i+1)] + rows[i]
-    dashboard_logging.debug("Sending Dashbaord update ...")
+    dashboard_logging.debug("Sending Dashboard update ...")
     dashboard_request(plots = rows, analysis=analysis)
 
 
@@ -137,7 +137,7 @@ def dashboard_request(plots, analysis):
         response = requests.patch(url, headers=headers, data=data)
         if response.status_code == 204:
             dashboard_status = "Connected"
-            dashboard_logging.info("Successfully updated endpoint: %s - ", url + str(response))
+            dashboard_logging.info("Successfully updated endpoint: %s", url)
         elif  response.status_code == 429:
             dashboard_status = "Too many Requests. Slow down. - "
             dashboard_logging.warning(dashboard_status + str(response))
@@ -145,10 +145,10 @@ def dashboard_request(plots, analysis):
             response.raise_for_status()
     except HTTPError:
         if response.status_code == 401:
-            dashboard_status = "Unauthorized. Possibly invalid API key? - "
+            dashboard_status = "Unauthorized. Possibly invalid API key?"
             dashboard_logging.warning(dashboard_status + str(response))
         else:
-            dashboard_status = "Unable to connect. - "
+            dashboard_status = "Unable to connect."
             dashboard_logging.error(dashboard_status + str(response))
     except requests.exceptions.ConnectionError:
         dashboard_status = "Connection Error. Chia Dashboard may not be responding."
